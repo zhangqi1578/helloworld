edefe738310a4ef550fc14e9e7a74e75adf7de52
zhangqi <827397328@qq.com>
2019-01-25 16:59:32 +0800

    add bbb.py

---
+++ aaa.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/19 14:22
+# @Author  : 'mucang'
+# @Email   : zhangqi02@mucang.cn
+# @File    : aaa.py
+# @Software: PyCharm Community Edition
+
+a = 1
+b = 2
---
+++ bbb.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/19 14:47
+# @Author  : 'mucang'
+# @Email   : zhangqi02@mucang.cn
+# @File    : bbb.py
+# @Software: PyCharm Community Edition
